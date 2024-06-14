e94ee656023e6da61fa2cd51d29f46d8442bed53
unknown <hastycodeandreh@gmail.com>
2024-06-14 18:58:02 +0300

    pattern

--- Stars.java
+++ Stars.java
@@ -4,18 +4,20 @@ public class Stars {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("How many stars? ");
+        System.out.print("Height? ");
         int input = Integer.valueOf(scanner.nextLine());
 
-        for (int i = 0; i < input; i++) {
+        for (int i = 1; i <= input; i++) {
 
-            for (int j = (input - i -1); j >= 0; j--) {
-                printSpace(j);
+            int stars = (i * 2) - 1;
+            int space = input - i;
+
+            if (stars % 2 == 1) {
+                printSpace(space);
+                printStars(stars);
+                System.out.println("");
             }
 
-            printStars(i);
-
-            System.out.println("");
         }
 
     }
