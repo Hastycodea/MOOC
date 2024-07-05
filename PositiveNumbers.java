b9edab8e9fe82f0be3df4f6154a167c949370258
unknown <hastycodeandreh@gmail.com>
2024-07-05 20:06:44 +0300

    .reduce, .distinct

--- PositiveNumbers.java
+++ PositiveNumbers.java
@@ -10,13 +10,30 @@ public class PositiveNumbers {
 
         Collections.addAll(answers, 1, -56, 8, 10, -78, 9, -34, 89, 33, -66);
 
+        int sum = answers.stream().reduce(0, (p, v) -> p + v);
+        System.out.println("Sum: " + sum);
+
+        answers.stream().forEach(System.out::println);
+        System.out.println();
+
         System.out.print(positive(answers).toString());
+        System.out.println();
+
+        //testing sorting
+        System.out.println("Testing sorting: ");
+
+        List<String> names = new ArrayList<>();
+
+        Collections.addAll(names, "Kimtai", "Robin", "Kimtai", "Juliani", "Maasai", "Kimtai");
+
+        names.stream().distinct().forEach(System.out::println);
 
     }
 
     public static List<Integer> positive(List<Integer> numbers) {
         List<Integer> result = numbers.stream()
                 .filter(n -> n > 0)
+                // any can be used
                 // .collect(Collectors.toList()); 
                 .collect(Collectors.toCollection(ArrayList::new));
         return result;
