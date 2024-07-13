2d37012f15ea515cd2befb01110a0fd2c73b0240
hastycodea <hastycodeandreh@gmail.com>
2024-07-13 18:43:51 +0300

    updated Count

--- Count.java
+++ Count.java
@@ -1,15 +1,22 @@
+import java.util.Scanner;
 
 public class Count{
     public static void main(String[] args) {
-	for(int i = 1; i <= 10; i++) {
+	Scanner scanner = new Scanner(System.in);
+
+	System.out.print("Enter a number: ");
+	int input = Integer.valueOf(scanner.nextLine());
+
+	System.out.println();
+	for(int i = 1; i <= input; i++) {
 		if(i > 1) {
 		   System.out.print(",");
 		}
 		System.out.print(i);	
 	}
-	System.out.println();
+	
 	System.out.println();
 
-	System.out.println("Hello World!");
+	System.out.println("Welcome to Big World!");
     }
 }
